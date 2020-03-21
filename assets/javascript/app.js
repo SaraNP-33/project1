e77818d96787dfaf5f7d57013f0672952e14b4de
hayjmuir <haymuir@gmail.com>
2020-03-21 11:39:49 -0700

    added some JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,18 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
 })
+
+var queryURL = "https://api.spoonacular.com/recipes/search?query=cheese&number=2?apiKey=84b16d63a51a4acb921b68dd844ae213"
+var nutritionURL = "https://api.spoonacular.com/recipes/parseIngredients?apiKey=84b16d63a51a4acb921b68dd844ae213"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {});
+
+
+
+  $.ajax({
+    url: nutritionURL,
+    method: "POST"
+  }).then(function(response) {});
