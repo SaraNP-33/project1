440d698ad864b4ce11fc2853b8f4cb2b8ddce337
Geiji <2is4you@gmail.com>
2020-03-23 15:17:14 -0700

    branch content

---
+++ assets/javascript/search.js
@@ -0,0 +1,33 @@
+}
+
+  let APIKEY = "b2abcf1f7aa34932bae5417c700268ec";
+  document.addEventListener("DOMContentLoaded", init);
+  function init() {
+    document.getElementById("btnSearch").addEventListener("click", ev => {
+      ev.preventDefault(); 
+     
+      let url = 
+      console.log(url);
+      fetch(url)
+        .then(response => response.json())
+        .then(content => {
+          
+          console.log(content.data);
+          console.log("META", content.meta);
+          let fig = document.createElement("figure");
+          let img = document.createElement("img");
+          let fc = document.createElement("figcaption");
+          img.src = content.data[0].images.downsized.url;
+          img.alt = content.data[0].title;
+          fc.textContent = content.data[0].title;
+          fig.appendChild(img);
+          fig.appendChild(fc);
+          let out = document.querySelector(".out");
+          out.insertAdjacentElement("afterbegin", fig);
+          document.querySelector("#search").value = "";
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    });
+  }
