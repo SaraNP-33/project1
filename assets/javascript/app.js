e073585d742557524807cdbb326ec945de69fb5b
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-03-23 15:06:45 -0700

    materialize fixing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,11 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
 })
+
+//ajax call to get photo + name to populate card
+
+//second ajax call to grab the rest of the items (recipe, video, grocery list) to the user -link it to the link and show it on another page. 
+// look up modal (materialize has modals)
+
+// use Api ingredient list options to have the user choose their ingredients. 
+//dispaly on html as checkboxes - modal when clicking on the link to choose ingredients. 
