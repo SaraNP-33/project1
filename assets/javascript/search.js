3ca7ca6265b434c7bdbe1b3898893d7f33ddcd2a
Geiji <2is4you@gmail.com>
2020-03-23 21:56:50 -0700

    search java code

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,33 +1,76 @@
-}
+
 
   let APIKEY = "b2abcf1f7aa34932bae5417c700268ec";
-  document.addEventListener("DOMContentLoaded", init);
-  function init() {
-    document.getElementById("btnSearch").addEventListener("click", ev => {
-      ev.preventDefault(); 
-     
-      let url = 
-      console.log(url);
-      fetch(url)
-        .then(response => response.json())
-        .then(content => {
-          
-          console.log(content.data);
-          console.log("META", content.meta);
-          let fig = document.createElement("figure");
-          let img = document.createElement("img");
-          let fc = document.createElement("figcaption");
-          img.src = content.data[0].images.downsized.url;
-          img.alt = content.data[0].title;
-          fc.textContent = content.data[0].title;
-          fig.appendChild(img);
-          fig.appendChild(fc);
-          let out = document.querySelector(".out");
-          out.insertAdjacentElement("afterbegin", fig);
-          document.querySelector("#search").value = "";
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    });
+  var queryURL = "https://api.spoonacular.com/recipes/complexSearch?query="+"&addRecipeNutrition=true&number=2&apiKey="+APIKEY
+
+function recipeSearch(){
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+   console.log(response);
+    console.log(queryURL);
+   
+  for (i = 0; i < 2; i++) {
+    var recipes = response.results;
+
+    var row = $();
+
+    var col = $();
+
+    var card = $("<div class=row>","div class=col s12 m7>","<div class=card>","<div class=card-image>","<div class=card-title","<div class=card-content>","<div class=card-action");
+
+    var cardimage = $();
+
+    var cardtitle = $(");
+
+    var cardcontent = $();
+
+    var cardaction = $();
+
+
   }
+
+  var image = $("<img>").attr("src",response.results.image);
+  $(".card-image").append(image);
+
+  //variable to store Time to Prepare//
+
+  var preparetime = $("<p>").text(response.results.preparationMinutes);
+  
+  //attach to html//
+
+  $(".card-content").append(preparetime);
+
+  //variable to store Time to Cook//
+
+  var cooktime = $("<p>").text(response.results.cookingMinutes);
+
+  //attach to html//
+
+  $(".card-content").append(cooktime);
+
+  //variable to store ready in minutes//
+
+  var readyminutes = $("<p>").text(response.results.readyInMinutes);
+console.log(readyminutes);
+  //attach to html//
+
+  $(".card-content").append(readyminutes);
+
+  //variable to store summery//
+
+  var summary = $("<p>").text(response.results.summary);
+
+  //variable to store link//
+
+var link = $("<a>").attr("href",response.results.soureUrl);
+
+//attach to html//
+
+$(".card-action").append(link);
+});
+}
+
+$("#searchBar").on("click",recipeSearch);
